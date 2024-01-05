33010980d901cb85bf79528ef4f25229d6da89da
Victor Campos <victorde.ac@gmail.com>
2024-01-05 18:29:58 -0300

    up until chapter 12 mutations

--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -1,8 +1,23 @@
 'use client';
 
 import { MagnifyingGlassIcon } from '@heroicons/react/24/outline';
+import { useSearchParams, usePathname, useRouter } from 'next/navigation';
 
 export default function Search({ placeholder }: { placeholder: string }) {
+  const searchParams = useSearchParams();
+  const pathname = usePathname();
+  const { replace } = useRouter();
+  function handleSearch(term: string) {
+    const params = new URLSearchParams(searchParams);
+    params.set('page', "1");
+    if (term) {
+      params.set('query', term);
+    } else {
+      params.delete('query');
+    }
+    replace(`${pathname}?${params.toString()}`);
+  }
+  const debouncedHandleSearch = debounce(handleSearch, 300);
   return (
     <div className="relative flex flex-1 flex-shrink-0">
       <label htmlFor="search" className="sr-only">
@@ -11,8 +26,18 @@ export default function Search({ placeholder }: { placeholder: string }) {
       <input
         className="peer block w-full rounded-md border border-gray-200 py-[9px] pl-10 text-sm outline-2 placeholder:text-gray-500"
         placeholder={placeholder}
+        onChange={(event) => debouncedHandleSearch(event.target.value)}
+        defaultValue={searchParams.get('query')?.toString()}
       />
       <MagnifyingGlassIcon className="absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
     </div>
   );
+}
+
+function debounce(func: any, delay: number) {
+  let timeoutId: any;
+  return function debouced(...args: any[]) {
+    clearInterval(timeoutId);
+    timeoutId = setTimeout(() => func(args), delay, args);
+  };
 }
